151b9c95ad715a88510f5f59da7a034c54e33612
Simon Vigneux <svigneux@kronostechnologies.com>
2014-07-03 12:34:44 -0400

    installation script

--- install/linux.sh
+++ install/linux.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
-aptitude install git python-pip -y
-pip install gitpython pyyaml
+sudo aptitude install git python-pip -y
 
-# GitPython
-pip install gitpython==0.3.2.RC1 pyyaml==3.11
+sudo pip install gitpython==0.3.2.RC1 pyyaml==3.11
+
+# apply bugfix to gitpython
+
+sudo sed -i "523s/Total'):$/Total'\) or line.startswith('\ ='\):/" /usr/local/lib/python2.7/dist-packages/git/remote.py
 
 sudo ln -s /opt/devman/devman /usr/local/bin/devman
