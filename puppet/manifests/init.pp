fcefedadee8ed291ec9b18515f437073cfc97de3
Simon Vigneux <svigneux@kronostechnologies.com>
2015-09-20 21:47:27 -0400

    wip: some hack with librarian puppet

---
+++ puppet/manifests/init.pp
@@ -0,0 +1,60 @@
+# == Class: devman
+#
+# Full description of class devman here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { 'devman':
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2015 Your name here, unless otherwise noted.
+#
+class devman ($folder, $user){
+
+  # install devman
+
+  vcsrepo { "${folder}/devman":
+    ensure   => present,
+    provider => git,
+    user     => $user,
+    source   => 'git@github.com:kronostechnologies/devman.git',
+  }
+
+  case $operatingsystem {
+    'Darwin'            : { $devman_install = "${folder}/devman/install-mac.sh" }
+    'RedHat', 'CentOS'  : { $devman_install = "${folder}/devman/install-linux.sh" }
+    /^(Debian|Ubuntu)$/ : { $devman_install = "${folder}/devman/install-linux.sh" }
+    default             : { fail('not supported') }
+  }
+
+  exec { $devman_install:
+    user    => $user,
+    creates => "/home/${user}/bin/devman"
+  }
+}
