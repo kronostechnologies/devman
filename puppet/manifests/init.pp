253f125884a828aac550c6cd9e7b304d4d21b6b5
Simon Vigneux <svigneux@kronostechnologies.com>
2015-09-22 22:05:48 -0400

    remove some text from devman module

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,43 +1,6 @@
-# == Class: devman
-#
-# Full description of class devman here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'devman':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
+# install devman
 class devman ($folder, $user, $config_file){
 
-  # install devman
   case $operatingsystem {
     'Darwin'            : { $devman_install = "${folder}/devman/install-mac.sh" }
     'RedHat', 'CentOS'  : { $devman_install = "${folder}/devman/install-linux.sh" }
@@ -64,9 +27,9 @@ class devman ($folder, $user, $config_file){
     creates => "/home/${user}/bin/devman"
   }
 
-
   file { "${folder}/devman/repos.yaml":
     ensure => 'link',
     target => $config_file,
   }
+
 }
